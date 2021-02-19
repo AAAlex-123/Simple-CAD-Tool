b78dadb43eb3ae60cf25c1c4d779232ac62fc0f2
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-02-19 12:28:04 +0200

    U Factory: populated with more methods

--- src/components/ComponentFactory.java
+++ src/components/ComponentFactory.java
@@ -3,10 +3,20 @@ package components;
 @SuppressWarnings("javadoc")
 public class ComponentFactory {
 
-	public static InputPin createInputPin(boolean active) {
-		return new InputPin(active);
+	// PINS
+	public static Pin createPin() {
+		return new Pin();
 	}
 
+	public static InputPin createInputPin() {
+		return new InputPin();
+	}
+
+	public static OutputPin createOutputPin() {
+		return new OutputPin();
+	}
+
+	// CONNECTIONS
 	public static Branch connectPins(Pin p1, Pin p2) {
 		return new Branch(p1, p2);
 	}
@@ -19,6 +29,24 @@ public class ComponentFactory {
 		return new Branch(g.getAvailableOut(), p);
 	}
 
+	// GATES
+	public static Gate createGate(Pin[] in, Pin[] out) {
+		return new Gate(in, out);
+	}
+
+	public static Gate createAND(int in) {
+		return new GateAND(in);
+	}
+
+	public static Gate createOR(int in) {
+		return new GateOR(in);
+	}
+
+	public static Gate createNOT() {
+		return new GateNOT();
+	}
+
+	// MISCELLANEOUS
 	public static void clearGate(Gate g) {
 		g.clear();
 	}
