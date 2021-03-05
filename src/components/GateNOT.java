148272542e1016676afc266b714d7ba3ef53d1c6
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-03-05 16:22:34 +0200

    U GateNOT: now final

--- src/components/GateNOT.java
+++ src/components/GateNOT.java
@@ -3,12 +3,12 @@ package components;
 // A Primitive Gate that maps the input to its logical not.
 final class GateNOT extends PrimitiveGate {
 
-	public GateNOT() {
+	GateNOT() {
 		super(1, 1);
 	}
 
 	@Override
-	protected void calculateOutput() {
+	void calculateOutput() {
 		boolean res = !inputPins[0].active;
 		outputPins[0].wake_up(res);
 	}
