07bdfcea4e2b05c743e1128c5e219e00d8051083
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-03-11 19:42:23 +0200

    drawing and new abstract methods

--- src/components/OutputPin.java
+++ src/components/OutputPin.java
@@ -1,11 +1,16 @@
 package components;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
 import exceptions.ComponentNotFoundException;
 
 // A Component with no output; only the client can get it.
 final class OutputPin extends Component {
 
 	private Branch inputBranch;
+	private boolean active;
 
 	private Gate outerGate;
 	private int outerGateIndex;
@@ -21,9 +26,22 @@ final class OutputPin extends Component {
 		// propagate signal only if it's different
 		if (active != newActive) {
 			active = newActive;
+			// repaint();
 			if (outerGate != null)
 				outerGate.outputChanged(outerGateIndex);
 		}
+	}
+
+	@Override
+	void destroy() {
+		if (inputBranch != null)
+			inputBranch.destroy();
+	}
+
+	@Override
+	boolean getActive(int index) {
+		checkIndex(index, 1);
+		return active;
 	}
 
 	@Override
@@ -31,7 +49,7 @@ final class OutputPin extends Component {
 		checkIndex(index, 1);
 		checkChangeable();
 		inputBranch = b;
-		wake_up(inputBranch.active, index);
+		wake_up(inputBranch.getActive(0), index);
 	}
 
 	@Override
@@ -39,12 +57,12 @@ final class OutputPin extends Component {
 		checkIndex(index, 1);
 		checkChangeable();
 		if (inputBranch != b)
-			throw new ComponentNotFoundException(b);
+			throw new ComponentNotFoundException(b, this);
 
 		inputBranch = null;
 	}
 
-	// proper way for the client to get output
+	// proper way for the client (the Factory) to get output
 	boolean getActive() {
 		checkChangeable();
 		return active;
@@ -56,5 +74,27 @@ final class OutputPin extends Component {
 		changeable = false;
 		outerGate = g;
 		outerGateIndex = index;
+	}
+
+	@Override
+	void draw(Graphics g) {
+		g.setColor(active ? Color.yellow : Color.black);
+		g.fillRect(0, 0, getWidth() - 1, getHeight() - 1);
+		g.setColor(Color.red);
+		// g.drawString("OUT", getWidth() / 2, getHeight() / 2);
+	}
+
+	@Override
+	void updateOnMovement() {
+		if (inputBranch != null)
+			inputBranch.updateOnMovement();
+	}
+
+	@Override
+	Point getBranchCoords(Branch b, int index) {
+		checkIndex(index, 1);
+		if (b == inputBranch)
+			return new Point(getX(), getY() + (getHeight() / 2));
+		throw new ComponentNotFoundException(b, this);
 	}
 }
