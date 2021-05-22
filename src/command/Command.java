3b48eb0d5bfcf46b3379116323f2789972c82a1d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-22 09:27:04 +0300

    interface refinement, static factory methods

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,63 +1,105 @@
-package application;
+package command;
 
 import java.awt.Frame;
 import java.io.Serializable;
+import java.util.List;
+
+import application.Application;
+import application.Undoable;
+import components.ComponentType;
+import requirement.Requirements;
 
 /** An implementation of the Undoable interface, specific to this Application */
-abstract class Command implements Undoable, Serializable {
+public abstract class Command implements Undoable, Serializable {
 
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 3L;
+
+	/**
+	 * Creates a Command that creates a Component of the given
+	 * {@code ComponentType}.
+	 * 
+	 * @param app           the Command's context
+	 * @param componentType the type of the Component
+	 * @return the Command
+	 */
+	public static Command create(Application app, ComponentType componentType) {
+		return new CreateCommand(app, componentType);
+	}
+
+	/**
+	 * Creates a Command that creates a composite Gate.
+	 * 
+	 * @param app         the Command's context
+	 * @param commands    the Command's instructions to create the composite
+	 * @param description the Command's description
+	 * @return the Command
+	 */
+	public static Command create(Application app, List<Command> commands, String description) {
+		return new CreateGateCommand(app, commands, description);
+	}
+
+	/**
+	 * Creates a Command that deletes a Component.
+	 * 
+	 * @param app the Command's context
+	 * @return the Command
+	 */
+	public static Command delete(Application app) {
+		return new DeleteCommand(app);
+	}
 
 	/** The Command's requirements; what it needs to execute. */
-	Requirements<String> requirements;
+	protected Requirements<String> requirements;
 
 	/** The Command's context; where it will act. */
-	transient Application context;
+	protected transient Application context;
 
 	/**
 	 * Constructs the Command with the given {@code Application}.
 	 *
 	 * @param app the Command's context
 	 */
-	Command(Application app) {
+	public Command(Application app) {
 		context = app;
 		requirements = new Requirements<>();
 	}
+
+	/**
+	 * Clones the Command so that the execution of this Command doesn't affect
+	 * future executions.
+	 *
+	 * @return the cloned Command
+	 */
+	@Override
+	public abstract Command clone();
 
 	/**
 	 * Fulfils the Command's requirements with a dialog.
 	 * 
 	 * @param parent the parent of the dialog
 	 */
-	final void fillRequirements(Frame parent) {
-		requirements.fulfillWithDialog(parent, desc());
+	public final void fillRequirements(Frame parent) {
+		requirements.fulfillWithDialog(parent, toString());
 	}
 
 	/**
-	 * Clones the Command so that the future execution of this Command doesn't
-	 * affect future executions. Should be the same as myclone(false).
-	 *
-	 * @return the cloned Command
+	 * Returns whether or not this Command is ready to execute.
+	 * 
+	 * @return true if ready to execute, false otherwise
 	 */
-	abstract Command myclone();
-
-	/**
-	 * Clones the command but keeps some information.
-	 * <p>
-	 * yes this is spaghetti
-	 *
-	 * @param keepIdAndReqs whether or not to retain some information
-	 * @return the cloned command
-	 */
-	Command myclone(@SuppressWarnings("unused") boolean keepIdAndReqs) {
-		throw new RuntimeException("not used lmao");
+	public final boolean canExecute() {
+		return requirements.fulfilled();
 	}
 
-	/** @return a description for this Command */
-	abstract String desc();
+	/**
+	 * Sets the Command's context.
+	 * 
+	 * @param c the context
+	 */
+	public final void context(Application c) {
+		context = c;
+	}
 
 	@Override
-	public String toString() {
-		return desc();
-	}
+	public abstract String toString();
 }
