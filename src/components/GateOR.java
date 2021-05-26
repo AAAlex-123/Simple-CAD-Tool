511a58c3258d07b9c6807f8aca5cc8d3c78ec741
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-26 08:55:38 +0300

    sprite

--- src/components/GateOR.java
+++ src/components/GateOR.java
@@ -1,9 +1,33 @@
 package components;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Function;
+
+import javax.imageio.ImageIO;
+
 /** A Primitive Gate that maps the inputs to their logical {@code or}. */
 final class GateOR extends PrimitiveGate {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
+
+	private static final String sprite = application.Application.component_icon_path + "gate_or.png";
+	private static final BufferedImage image;
+
+	static {
+		BufferedImage temp = null;
+		File file = null;
+
+		try {
+			file = new File(sprite);
+			temp = ImageIO.read(file);
+		} catch (@SuppressWarnings("unused") IOException e) {
+			System.err.printf("Could not load image %s", file);
+		}
+
+		image = temp;
+	}
 
 	/**
 	 * Constructs the OR Gate with the given number of inputs and one output.
@@ -20,7 +44,7 @@ final class GateOR extends PrimitiveGate {
 	}
 
 	@Override
-	void calculateOutput() {
+	protected void calculateOutput() {
 		if (checkBranches()) {
 			boolean res = false;
 
@@ -31,5 +55,15 @@ final class GateOR extends PrimitiveGate {
 			// and outputs the result
 			outputPins[0].wake_up(res);
 		}
+	}
+
+	@Override
+	protected BufferedImage getImage() {
+		return image;
+	}
+
+	@Override
+	protected Function<Integer, Integer> dxi() {
+		return i -> (int) (0.55 * Math.cos(1.2 * Math.asin(1 - (dyi().apply(i) / 20.0))) * 20);
 	}
 }
