32adc6c29bd16a69af0f74eca14af219839b5b5f
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-27 20:43:58 +0300

    Delete application directory

--- application/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Defines a set of classes that together create an interactive Application with
- * a UI. The user may run and use it to see their dream circuit come to life.
- */
-package application;
