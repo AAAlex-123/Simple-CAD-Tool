bcf7527eea502e3439222bc1f9c8e87834ad6360
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-05-27 08:18:53 +0300

    app created outside invokelater

--- src/App.java
+++ src/App.java
@@ -14,9 +14,7 @@ public final class App {
 	 * @param args command line arguments (not used)
 	 */
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			Application app = new Application();
-			app.run();
-		});
+		Application app = new Application();
+		SwingUtilities.invokeLater(() -> app.run());
 	}
 }
