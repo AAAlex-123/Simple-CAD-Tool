2aa7a0e659f520e6301539117ec93a4de7f90b6a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-06-29 08:48:53 +0300

    Added StringConstants.java

---
+++ src/application/StringConstants.java
@@ -0,0 +1,19 @@
+package application;
+
+/**
+ * A collection of String Constants used throughout the project.
+ *
+ * @author alexm
+ */
+public final class StringConstants {
+	private StringConstants() {}
+
+	/** Location of the menu icons */
+	public static final String menu_icon_path = "assets\\menu_icons\\";
+
+	/** Location of the component images */
+	public static final String component_icon_path = "assets\\component_icons\\";
+
+	/** Location of the user-created files */
+	public static final String user_data = "user_data\\";
+}
