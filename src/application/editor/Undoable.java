7aea6262d5f5cbd36b138d1f2f2d2732399858f1
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-06-29 09:52:16 +0300

    fixed package

--- src/application/editor/Undoable.java
+++ src/application/editor/Undoable.java
@@ -1,11 +1,15 @@
-package application;
+package application.editor;
 
-/** An Interface for objects that can be executed and un-executed */
+/**
+ * An Interface for objects that can be executed and un-executed
+ *
+ * @author alexm
+ */
 public interface Undoable {
 
 	/**
 	 * Executes the Undoable
-	 * 
+	 *
 	 * @throws Exception when an exception occurred
 	 */
 	void execute() throws Exception;
