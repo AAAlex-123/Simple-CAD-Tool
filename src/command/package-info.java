5c46377797bc83c328b807a3786ff2a91ffdc8f2
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-06-30 17:27:10 +0300

    author and doc

--- src/command/package-info.java
+++ src/command/package-info.java
@@ -1,6 +1,8 @@
 /**
- * Defines a hierarchy of classes that encapsulate different commands that can
- * be executed and un-executed on an Application.
+ * Defines a hierarchy of {@code Commands} that encapsulate different commands
+ * that can be executed and un-executed on an Editor. These {@code Commands}
+ * create or delete {@link components.Component Components} from
+ * {@link application.editor.Editor Editors}.
  *
  * @author alexm
  */
