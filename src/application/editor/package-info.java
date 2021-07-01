59d4a6ea73aabf99e807be5e6883abe08119deb5
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-01 18:11:52 +0300

    doc

--- src/application/editor/package-info.java
+++ src/application/editor/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Set of classes that define an Editor, the part of the Application responsible
- * for editing a file.
+ * Set of classes that define an {@link application.editor.Editor Editor}, the
+ * part of the Application responsible for editing a file.
  *
  * @author alexm
  */
