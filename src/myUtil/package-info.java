e4a5d3c3af178246020d5ead6713efaf73d5fe30
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-01 19:00:03 +0300

    doc + stringgenerator

--- src/myUtil/package-info.java
+++ src/myUtil/package-info.java
@@ -1,5 +1,9 @@
 /**
- * Defines a set of utility functions used throughout the project. All the
- * functions are located in the {@link myUtil.Utility} class.
+ * Defines a set of utility functions used throughout the project. They are
+ * located in the {@link myUtil.Utility Utility} class. Additionally defines a
+ * {@link myUtil.StringGenerator StringGenerator} which formats a given String
+ * with a number to generate a sequence of other Strings.
+ *
+ * @author alexm
  */
 package myUtil;
