da553ee9435aef8abe5f2c7013195cf6e109302c
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-07 16:00:02 +0300

    same as Component

--- src/components/GateXOR.java
+++ src/components/GateXOR.java
@@ -1,36 +1,15 @@
 package components;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.function.Function;
-
-import javax.imageio.ImageIO;
-
-import application.StringConstants;
-
-/** A Primitive Gate that maps the inputs to their logical {@code xor}. */
+/**
+ * A Primitive Gate that maps the inputs to their logical {@code xor}.
+ *
+ * @author alexm
+ */
 final class GateXOR extends PrimitiveGate {
 
-	private static final long serialVersionUID = 3L;
+	private static final long serialVersionUID = 4L;
 
-	private static final String sprite = StringConstants.COMPONENT_ICON_PATH + "gate_xor.png";
-
-	private static final BufferedImage image;
-
-	static {
-		BufferedImage temp = null;
-		File file = null;
-
-		try {
-			file = new File(sprite);
-			temp = ImageIO.read(file);
-		} catch (IOException e) {
-			System.err.printf("Could not load image %s", file);
-		}
-
-		image = temp;
-	}
+	private final ComponentGraphic g;
 
 	/**
 	 * Constructs the XOR Gate with the given number of inputs and one output.
@@ -39,6 +18,7 @@ final class GateXOR extends PrimitiveGate {
 	 */
 	GateXOR(int in) {
 		super(in, 1);
+		g = new GateXORGraphic(this);
 	}
 
 	@Override
@@ -61,12 +41,7 @@ final class GateXOR extends PrimitiveGate {
 	}
 
 	@Override
-	protected BufferedImage getImage() {
-		return image;
-	}
-
-	@Override
-	protected Function<Integer, Integer> dxi() {
-		return i -> (int) (0.55 * Math.cos(1.2 * Math.asin(1 - (dyi().apply(i) / 20.0))) * 20);
+	public ComponentGraphic getGraphics() {
+		return g;
 	}
 }
