3f40c27e879f1702ff212bebef9e01f216add3ff
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-17 13:16:21 +0300

    Create CommandStrings.java

---
+++ src/localisation/CommandStrings.java
@@ -0,0 +1,24 @@
+package localisation;
+
+/**
+ * Shared Strings of the command package to be externalised once from here.
+ *
+ * @author alexm
+ */
+@SuppressWarnings("javadoc")
+public final class CommandStrings {
+
+	public static final String NAME = Languages.getString("CommandStrings.0"); //$NON-NLS-1$
+	public static final String ID = Languages.getString("CommandStrings.1"); //$NON-NLS-1$
+	public static final String IN_ID = Languages.getString("CommandStrings.2"); //$NON-NLS-1$
+	public static final String IN_INDEX = Languages.getString("CommandStrings.3"); //$NON-NLS-1$
+	public static final String OUT_ID = Languages.getString("CommandStrings.4"); //$NON-NLS-1$
+	public static final String OUT_INDEX = Languages.getString("CommandStrings.5"); //$NON-NLS-1$
+	public static final String IN_COUNT = Languages.getString("CommandStrings.6"); //$NON-NLS-1$
+	public static final String DELETE_STR = Languages.getString("CommandStrings.7"); //$NON-NLS-1$
+	public static final String CREATE_STR = Languages.getString("CommandStrings.8"); //$NON-NLS-1$
+
+	/* Don't let anyone initialise this class */
+	private CommandStrings() {}
+
+}
