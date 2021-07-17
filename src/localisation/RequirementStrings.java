e7fca3f0d20dda5416087d635f919aa7d2bfd047
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-17 18:44:58 +0300

    Create RequirementStrings.java

---
+++ src/localisation/RequirementStrings.java
@@ -0,0 +1,10 @@
+package localisation;
+
+public final class RequirementStrings {
+
+	public static final String ON = "on";
+	public static final String OFF = "off";
+
+	/* Don't let others initialize this class */
+	private RequirementStrings() {}
+}
