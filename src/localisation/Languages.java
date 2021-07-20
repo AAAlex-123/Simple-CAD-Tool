ffe84b18d14a9e358a71eec39c48a535d9457834
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-20 19:14:56 +0300

    replaced map with a Properties object
    cleaned up a lot of stuff

--- src/localisation/Languages.java
+++ src/localisation/Languages.java
@@ -9,18 +9,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.MissingResourceException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 
+import myUtil.OrderedProperties;
 import myUtil.Utility;
 
 /**
@@ -31,35 +30,20 @@ import myUtil.Utility;
  */
 public class Languages {
 
-	private Languages() {}
-
-	/** The file that contains the language settings */
-	public static final String LANGUAGES_FILE = "program_data\\languages.properties"; //$NON-NLS-1$
-
+	/** The file containing information about the current language */
+	public static final String  FILE                = "program_data\\languages.properties"; //$NON-NLS-1$
 	private static final String LANGUAGES_DIRECTORY = "src\\localisation";                  //$NON-NLS-1$
 
-	private static final Map<String, String> map = new LinkedHashMap<String, String>() {
-														@Override
-														public String get(
-														        Object key) {
-															final String rv = super.get(
-															        key);
-															if (rv == null)
-																System.err.printf(
-																        "No value found for key %s%n",              //$NON-NLS-1$
-																        key);
-															return rv;
-														}
-													};
+	private static final Properties properties = new OrderedProperties();
 
-	private static final String LANGUAGE, COUNTRY, VARIANT;
-	private static final String LANGUAGE_STR, COUNTRY_STR, VARIANT_STR;
-
+	private static final String  LANGUAGE_STR, COUNTRY_STR, VARIANT_STR;
 	private static final String  regex;
 	private static final Pattern pattern;
 
 	private static final String BUNDLE_NAME = "localisation.language"; //$NON-NLS-1$
-	private static final ResourceBundle RESOURCE_BUNDLE;
+
+	private static String         LANGUAGE, COUNTRY, VARIANT;
+	private static ResourceBundle RESOURCE_BUNDLE;
 
 	static {
 		LANGUAGE_STR = "Language"; //$NON-NLS-1$
@@ -70,23 +54,23 @@ public class Languages {
 		        LANGUAGE_STR, COUNTRY_STR, VARIANT_STR);
 		pattern = Pattern.compile(regex);
 
-		try {
-			Languages.loadFromFile();
+		try (BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
+			properties.load(reader);
 		} catch (final FileNotFoundException e) {
-			System.err.printf(Languages.getString("StringConstants.2"), LANGUAGES_FILE); //$NON-NLS-1$
+			System.err.printf("File %s doesn't exist%n", FILE); //$NON-NLS-1$
 			System.exit(0);
 		} catch (final IOException e) {
 			System.err.printf(
-			        Languages.getString("StringConstants.3"), //$NON-NLS-1$
-			        LANGUAGES_FILE);
+			        "Error while reading from file %s. Inform the developer about 'Settings.static-IO'%", //$NON-NLS-1$
+			        FILE);
 			System.exit(0);
-		} finally {
-			LANGUAGE = map.get(LANGUAGE_STR);
-			COUNTRY = map.get(COUNTRY_STR);
-			VARIANT = map.get(VARIANT_STR);
-			RESOURCE_BUNDLE = ResourceBundle.getBundle(Languages.BUNDLE_NAME,
-			        new Locale(LANGUAGE, COUNTRY, VARIANT));
 		}
+
+		LANGUAGE = properties.getProperty(LANGUAGE_STR);
+		COUNTRY = properties.getProperty(COUNTRY_STR);
+		VARIANT = properties.getProperty(VARIANT_STR);
+		RESOURCE_BUNDLE = ResourceBundle.getBundle(Languages.BUNDLE_NAME,
+		        new Locale(LANGUAGE, COUNTRY, VARIANT));
 	}
 
 	/**
@@ -103,6 +87,7 @@ public class Languages {
 			return '!' + key + '!';
 		}
 	}
+
 
 	/**
 	 * Displays a pop-up dialog to edit the language settings and writes them to the
@@ -144,46 +129,22 @@ public class Languages {
 		}
 
 		Locale chosen  = (Locale) JOptionPane.showInputDialog(frame,
-		        Languages.getString("Languages.3"), Languages.getString("Languages.4"), //$NON-NLS-1$ //$NON-NLS-2$
+		        Languages.getString("Languages.3"), Languages.getString("Languages.4"),                            //$NON-NLS-1$ //$NON-NLS-2$
 		        JOptionPane.PLAIN_MESSAGE, null, locales.toArray(), locales.get(0));
-		Locale current = new Locale(map.get(LANGUAGE_STR), map.get(COUNTRY_STR),
-		        map.get(VARIANT_STR));
+		Locale current = new Locale(properties.getProperty(LANGUAGE_STR),
+		        properties.getProperty(COUNTRY_STR), properties.getProperty(VARIANT_STR));
 
 		if ((chosen == null) || chosen.equals(current))
 			return false;
 
-		System.out.printf("Chosen locale: %s%n", chosen); //$NON-NLS-1$
+		properties.setProperty(LANGUAGE_STR, chosen.getLanguage());
+		properties.setProperty(COUNTRY_STR, chosen.getCountry());
+		properties.setProperty(VARIANT_STR, chosen.getVariant());
 
-		map.put(LANGUAGE_STR, chosen.getLanguage());
-		map.put(COUNTRY_STR, chosen.getCountry());
-		map.put(VARIANT_STR, chosen.getVariant());
-
-		writeToFile();
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE))) {
+			properties.store(writer, "settings lmao"); //$NON-NLS-1$
+		}
 		return true;
-	}
-
-	private static void writeToFile() throws IOException {
-		try (BufferedWriter writer = new BufferedWriter(
-		        new FileWriter(Languages.LANGUAGES_FILE))) {
-			for (final Entry<String, String> e : map.entrySet()) {
-				writer.write(String.format("%s=%s%n", e.getKey(), e.getValue())); //$NON-NLS-1$
-			}
-		}
-	}
-
-	private static void loadFromFile() throws FileNotFoundException, IOException {
-
-		try (BufferedReader reader = new BufferedReader(
-		        new FileReader(Languages.LANGUAGES_FILE))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				// lines starting with '#' are comments
-				if (!line.equals("") && !line.startsWith("#")) { //$NON-NLS-1$ //$NON-NLS-2$
-					final String[] parts = line.split("="); //$NON-NLS-1$
-					map.put(parts[0], parts.length == 2 ? parts[1] : ""); //$NON-NLS-1$
-				}
-			}
-		}
 	}
 
 	private static String emptyStringIfNull(String s) {
