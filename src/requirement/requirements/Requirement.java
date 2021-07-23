a7f1882ab00320dc4b0a65f8b3efdbd2d3ab0e2d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-07-23 21:06:17 +0300

    Changed Requirement to an abstract superclass
    Now is an abstract class. Almost every method is final, except from getGraphics(), isValidValue(Object) and resetValue(). These three methods fully describe how each concrete subclass is different from every other subclass while also functioning as a Requirement. The previously-private fields are now, of course, protected. The copy constructor was replaced with the Cloneable interface and the clone() method as abstract classes don't work well with constructors. Removed String-specific behavior, which can be fully defined by isValidValue(Object) and resetValue(). Greatly improved documentation.

--- src/requirement/requirements/Requirement.java
+++ src/requirement/requirements/Requirement.java
@@ -6,173 +6,246 @@ import static localisation.RequirementStrings.YES;
 import java.io.Serializable;
 
 import localisation.Languages;
+import requirement.exceptions.LockedRequirementException;
+import requirement.exceptions.UnsupportedGraphicException;
+import requirement.graphics.AbstractRequirementGraphic;
 
 /**
- * A class representing a key-value pair where the key is a string and the value
- * is the required object. Can be used to pass around objects that are required
- * by other objects.
+ * A class encapsulating the process where:
+ * <ul>
+ * <li>object A needs object B in order to carry out an operation
+ * <li>object C has access to / can construct object B
+ * <li>object C provides B to A thereby fulfilling A's requirement
+ * <li>object A receives object B and can carry out its operation
+ * </ul>
  * <p>
- * When the required object is of type {@code String}, restrictions may
- * optionally be enforced using the appropriate {@link StringType}.
- *
- * @param <V> the type of the Requirement's value
+ * An {@code AbstractRequirement} ({@code Requirement}) encapsulates this
+ * behaviour (and extends it to some extent) providing the means for object A to
+ * create the Requirement that object C will fulfil with object B, which A will
+ * then retrieve.
+ * <p>
+ * A Requirement may optionally be accompanied by a
+ * {@link requirement.graphics.AbstractRequirementGraphic
+ * AbstractRequirementGraphic} object which is responsible for providing a GUI
+ * with which the user fulfils the Requirement. If such Graphic object isn't
+ * defined, the {@link AbstractRequirement#getGraphics getGraphics} method
+ * should throw an {@link requirement.exceptions.UnsupportedGraphicException}.
  *
  * @author alexm
  */
-public final class Requirement<V> implements Serializable {
+public abstract class AbstractRequirement implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = 6L;
 
-	private final String key;
-	private V            value;
-	private V            defaultValue;
-
-	private boolean fulfilled, finalised;
-
-	/** The (optional) type of String expected as value. Used when V == String. */
-	final StringType stringType;
+	/**
+	 * A short and descriptive name for the Requirement which may be displayed in a
+	 * Graphic object and is used in a {@link requirement.requirements.Requirements
+	 * Requirements} object to identify it.
+	 */
+	protected final String key;
 
 	/**
-	 * Constructs this Requirement with a {@code key}.
-	 *
-	 * @param key the key
+	 * The required object, the object that will be set when the Requirement is
+	 * fulfilled and will later be retrieved by another object to be used to carry
+	 * out another operation.
 	 */
-	public Requirement(String key) {
+	protected Object value;
+
+	/**
+	 * The default value of the Requirement. This (optional) value is used to when
+	 * resetting the Requirement and may also be the preset option in a Graphic
+	 * object.
+	 */
+	protected Object defaultValue;
+
+	/**
+	 * Indicates whether or a valid value for this Requirement has been provided. If
+	 * the Requirement is fulfilled, the object that created the Requirement may
+	 * retrieve its value and use it.
+	 */
+	protected boolean fulfilled;
+
+	/**
+	 * Indicates whether or not the value for this Requirement is final, meaning it
+	 * cannot be altered. A Graphic object may use this information to alter its
+	 * appearance if the Requirement is finalised.
+	 */
+	protected boolean finalised;
+
+	/**
+	 * Constructs this Requirement with the given {@code key}.
+	 *
+	 * @param key the key of this Requirement
+	 */
+	public AbstractRequirement(String key) {
 		this.key = key;
 		value = defaultValue = null;
 		fulfilled = finalised = false;
-		stringType = null;
+	}
+
+	@Override
+	public AbstractRequirement clone() {
+		try {
+			return (AbstractRequirement) super.clone();
+		} catch (CloneNotSupportedException e) {
+			return null;
+		}
 	}
 
 	/**
-	 * Constructs this Requirement with a {@code key} and a {@code type}.
+	 * Returns the {@code Graphic} for this Requirement. Since not all Requirements
+	 * require a Graphic to function, this method may be defined to lazily create
+	 * the Graphic object the first time it is needed.
 	 *
-	 * @param key  the key
-	 * @param type the type
+	 * @return the Graphic
 	 *
-	 * @see Requirement#stringType
+	 * @throws UnsupportedGraphicException if this Requirement doesn't support a
+	 *                                     Graphic object
 	 */
-	public Requirement(String key, StringType type) {
-		this.key = key;
-		value = defaultValue = null;
-		fulfilled = finalised = false;
-		stringType = type;
+	public abstract AbstractRequirementGraphic getGraphics() throws UnsupportedGraphicException;
+
+	/**
+	 * Calls {@link AbstractRequirement#getGraphics() getGraphics()} while also
+	 * {@link AbstractRequirementGraphic#update() updating} the {@code Graphic}.
+	 *
+	 * @return the Graphic
+	 *
+	 * @throws UnsupportedGraphicException if this Requirement doesn't support a
+	 *                                     Graphic object
+	 */
+	public final AbstractRequirementGraphic getGraphicsAndUpdate()
+	        throws UnsupportedGraphicException {
+		final AbstractRequirementGraphic g = getGraphics();
+		g.update();
+		return g;
 	}
 
 	/**
-	 * Copy constructor.
+	 * Returns the {@link AbstractRequirement#key key}
 	 *
-	 * @param other the object to be copied
+	 * @return the key
 	 */
-	public Requirement(Requirement<V> other) {
-		this(other.key, other.stringType);
-		fulfilled = other.fulfilled;
-		finalised = other.finalised;
-		value = other.value;
-		defaultValue = other.defaultValue;
-	}
-
-	/** @return the key */
-	public String key() {
+	public final String key() {
 		return key;
 	}
 
-	/** @return the value */
-	public V value() {
+	/**
+	 * Returns the {@link AbstractRequirement#value value}
+	 *
+	 * @return the value
+	 */
+	public final Object value() {
 		return value;
 	}
 
-	/** @return the default value */
-	V defaultValue() {
+	/**
+	 * Returns the {@link AbstractRequirement#defaultValue defaultValue}
+	 *
+	 * @return the defaultValue
+	 */
+	public final Object defaultValue() {
 		return defaultValue;
 	}
 
 	/**
-	 * Attempts to set the {@code default value} of this Requirement without marking
-	 * it as {@code fulfilled}. If {@code v} is a {@code String} additional
-	 * restrictions may be enforced according to the {@code stringType}.
+	 * Checks if {@code v} is a valid {@code value} for this Requirement.
+	 *
+	 * @param v the value
+	 *
+	 * @return {@code true} if {@code v} is valid, {@code false} otherwise
+	 */
+	protected abstract boolean isValidValue(Object v);
+
+	/**
+	 * Attempts to set the {@code defaultValue} of this Requirement <i>without</i>
+	 * marking it as {@code fulfilled}. If the {@code v} isn't valid, this method
+	 * does nothing.
 	 * <p>
 	 * This method is intended for offering a default value which must then be
 	 * explicitly set in order for the Requirement to be marked as fulfilled.
 	 *
 	 * @param v the default value of the required object
 	 */
-	public void offer(V v) {
+	public final void offer(Object v) {
 		if (finalised)
 			throw new LockedRequirementException(this);
 
-		if ((v instanceof String) && (stringType != null) && !stringType.isValid((String) v))
+		if (!isValidValue(v))
 			return;
 
 		defaultValue = v;
 	}
 
 	/**
-	 * Calls {@link Requirement#offer(Object) offer(V)} and additionally sets the
-	 * {@code value} of this Requirement, marking is as {@code fulfilled}.
+	 * Calls {@link ObjectRequirement#offer(Object) offer(Object)} and additionally
+	 * sets the {@code value} of this Requirement, marking is as {@code fulfilled}.
 	 * <p>
 	 * This method is intended for normal use, to specify that a value for this
-	 * Requirement exists.
+	 * Requirement exists and may be retrieved.
 	 *
 	 * @param v the value of the required object
 	 */
-	public void fulfil(V v) {
+	public final void fulfil(Object v) {
 		offer(v);
 		value = v;
-		fulfilled = !((v instanceof String) && (stringType != null)
-				&& !stringType.isValid((String) v));
+		fulfilled = isValidValue(v);
 	}
 
 	/**
-	 * Calls {@link Requirement#fulfil(Object) fulfil(V)} and additionally marks
-	 * this Requirement as finalised, meaning that its value cannot be altered.
+	 * Calls {@link ObjectRequirement#fulfil(Object) fulfil(Object)} and
+	 * additionally marks this Requirement as {@code finalised}.
 	 * <p>
 	 * This method is intended for providing a strict default or for ensuring the
 	 * value cannot be altered in the future.
 	 *
 	 * @param v the value of the required object
 	 */
-	public void finalise(V v) {
+	public final void finalise(Object v) {
 		fulfil(v);
 		finalised = true;
 	}
 
-	/** Clears this Requirement and its {@code value} resetting its state */
+	/** Clears this Requirement and its {@code value}, resetting its state */
 	public void clear() {
 		value = null;
 		fulfilled = false;
 		finalised = false;
 	}
 
-	/** Clears this Requirement and resets the {@code value} to its default */
-	@SuppressWarnings("unchecked")
-	public void reset() {
+	/** Resets the value of the Requirement */
+	protected abstract void resetValue();
+
+	/** Clears this Requirement and resets its {@code value} to the default */
+	public final void reset() {
 		clear();
-		if ((value instanceof String) && (defaultValue == null))
-			value = (V) ""; //$NON-NLS-1$
-		else
-			value = defaultValue;
+		resetValue();
+		getGraphics().reset();
 	}
 
-	/** @return {@code true} if fulfilled, {@code false} otherwise */
-	public boolean fulfilled() {
+	/**
+	 * Returns the value of {@link AbstractRequirement#fulfilled fulfilled}
+	 *
+	 * @return {@code fulfilled}
+	 */
+	public final boolean fulfilled() {
 		return fulfilled;
 	}
 
-	/** @return {@code true} if finalised, {@code false} otherwise */
-	public boolean finalised() {
+	/**
+	 * Returns the value of {@link AbstractRequirement#finalised finalised}
+	 *
+	 * @return {@code finalised}
+	 */
+	public final boolean finalised() {
 		return finalised;
 	}
 
 	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder(String.format(
-				Languages.getString("Requirement.1"), //$NON-NLS-1$
-				key, defaultValue, value, fulfilled() ? YES : NO, finalised() ? YES : NO));
-
-		if (stringType != null)
-			sb.append(String.format(Languages.getString("Requirement.2"), stringType)); //$NON-NLS-1$
-
+	public final String toString() {
+		final StringBuilder sb = new StringBuilder(
+		        String.format(Languages.getString("Requirement.1"), this.getClass().getSimpleName(), //$NON-NLS-1$
+		                key, defaultValue, value, fulfilled() ? YES : NO,
+		                finalised() ? YES : NO));
 		return sb.toString();
 	}
 }
