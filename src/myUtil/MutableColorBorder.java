f7d9e3d890ca14ba796a998180631d090eb61223
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-08-02 16:25:20 +0300

    Added doc

--- src/myUtil/MutableColorBorder.java
+++ src/myUtil/MutableColorBorder.java
@@ -15,16 +15,30 @@ class MutableColorBorder extends LineBorder {
 	private static final long serialVersionUID = 1649237099464980877L;
 	
 	private Color currentColor;
-
+	
+	/**
+	 * Creates a new line border with the specified color. 
+	 * The color can later be changed by calling the {@link #setColor(Color)} method.
+	 * 
+	 * @param color the original color of the border
+	 */
 	public MutableColorBorder(Color color) {
 		super(color);
 		currentColor = color;
 	}
-
+	
+	/**
+	 * Changes the color of the border.
+	 * 
+	 * @param newColor the new Color
+	 */
 	public void setColor(Color newColor) {
 		currentColor = newColor;
 	}
 	
+	/**
+	 * Used by the renderer. Do NOT call this method directly. Use {@link #setColor(Color)} instead.
+	 */
 	@Override
 	public void paintBorder(final Component c, final Graphics g, final int x, final int y, final int width, final int height) {
 		g.setColor(currentColor);
