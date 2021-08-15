b0351227b4bbfe6718696570af41023460502be6
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-08-15 17:10:39 +0300

    Runtime filtering

--- src/requirement/requirements/ComponentRequirement.java
+++ src/requirement/requirements/ComponentRequirement.java
@@ -7,13 +7,15 @@ import components.Component;
 import components.ComponentType;
 
 /**
- * A {@link ListRequirement} that handles components. It holds their IDs and
- * can perform filtering by taking into account the type of different components.
+ * A {@link ListRequirement} that handles components. Utilizes IDs and
+ * can perform filtering taking into account the state and purpose
+ * of different components.
  * 
  * @author dimits
  *
  */
 public class ComponentRequirement extends ListRequirement<String> {
+	private final Policy policy;
 	
 	/**
 	 * Constructs a {@link ListRequirement} with a list of visible components' IDs 
@@ -26,19 +28,32 @@ public class ComponentRequirement extends ListRequirement<String> {
 	 */
 	public ComponentRequirement(String key, List<Component> options, Policy policy) {
 		super(key, filterAndGetIDs(options, policy));
+		this.policy = policy;
 	}
 	
 	/**
-	 * Constructs a {@link ListRequirement} with a list of visible components' IDs.<br>
-	 * Consider {@link #ComponentRequirement(String, List, Policy)} for automatic 
-	 * component filtering.
+	 * Constructs a {@link ListRequirement} with a list of visible components' IDs 
+	 * filtering out unnecessary components.
 	 * 
 	 * @param key the {@link AbstractRequirement#key}
-	 * @param options a list with all visible components
+	 * @param policy a {@link Policy} used to filter out unnecessary components.
 	 * @return a list with the suitable components' IDs.
 	 */
-	public ComponentRequirement(String key, List<Component> options) {
-		super(key, filterAndGetIDs(options, Policy.ANY));
+	public ComponentRequirement(String key, Policy policy) {
+		super(key);
+		this.policy = policy;
+	}
+	
+	/**
+	 * Set the list with components at runtime.
+	 * Required to be called before every {@link #getOptions()} call.
+	 * 
+	 * @param options the list with components
+	 * 
+	 * @implNote internally calls {@link #setOptions(List)} after filtering
+	 */
+	public void setComponentOptions(List<Component> options) {
+		super.setOptions((filterAndGetIDs(options, policy)));
 	}
 	
 	/**
@@ -77,6 +92,8 @@ public class ComponentRequirement extends ListRequirement<String> {
 		INPUT, 
 		/**Get a list of components available for output.*/
 		OUTPUT, 
+		/**Get a list of all non-branch components*/
+		NONBRANCH,
 		/**Get a list of all components.*/
 		ANY
 	}
