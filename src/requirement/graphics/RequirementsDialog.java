f4cf9991d8a43c57a3adbb0aa1e5cea98a0538f9
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-08-22 15:00:51 +0300

    Error checking for empty list

--- src/requirement/graphics/RequirementsDialog.java
+++ src/requirement/graphics/RequirementsDialog.java
@@ -72,10 +72,6 @@ public final class RequirementsDialog extends JDialog {
 		setLayout(new BorderLayout());
 		setResizable(false);
 
-		// --- options panel (middle) ---
-		optionsPanel = new JPanel(new GridLayout(reqs.size(), 1, 0, 15));
-		Utility.foreach(reqs, r -> optionsPanel.add(r.getGraphicsAndUpdate()));
-
 		// --- buttons panel (bottom) ---
 		buttonsPanel = new JPanel(new FlowLayout());
 		buttonsPanel.add(okButton = new JButton(Languages.getString("RequirementsDialog.0"))); //$NON-NLS-1$
@@ -84,7 +80,17 @@ public final class RequirementsDialog extends JDialog {
 
 		sb = new StatusBar();
 		sb.addLabel("message"); //$NON-NLS-1$
-
+		
+		// --- options panel (middle) ---
+		optionsPanel = new JPanel(new GridLayout(reqs.size(), 1, 0, 15));
+		try {
+			Utility.foreach(reqs, r -> optionsPanel.add(r.getGraphicsAndUpdate()));
+		} catch (NoSuchElementException e) {
+			sb.setLabelText("message", TextType.FAILURE, e.getMessage());
+			okButton.setEnabled(false);
+			resetButton.setEnabled(false);
+		}
+		
 		// --- scroll pane (for options panel) ---
 		final JScrollPane jsp = new JScrollPane(optionsPanel);
 		jsp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
