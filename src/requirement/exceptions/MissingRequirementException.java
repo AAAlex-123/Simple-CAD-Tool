2aa8b0c3433e43a12d8a9f93733b923cf6d9bb41
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-25 11:27:11 +0300

    doc

--- src/requirement/exceptions/MissingRequirementException.java
+++ src/requirement/exceptions/MissingRequirementException.java
@@ -1,8 +1,8 @@
 package requirement.exceptions;
 
 /**
- * Thrown when no {@code Requirement} with the {@code key} can be found within a
- * collection of Requirements.
+ * Thrown when no {@code Requirement} with a specific {@code key} can be found
+ * within a collection of Requirements.
  *
  * @author alexm
  */
@@ -11,9 +11,9 @@ public class MissingRequirementException extends RuntimeException {
 	/**
 	 * Constructs the Exception with information about the {@code key}.
 	 *
-	 * @param key the key for which there is no Requirement
+	 * @param key the key for which no Requirement exists
 	 */
 	public MissingRequirementException(String key) {
-		super(String.format("No Requirement with key %s exists", key)); //$NON-NLS-1$
+		super(String.format("No Requirement with key '%s' exists", key)); //$NON-NLS-1$
 	}
 }
