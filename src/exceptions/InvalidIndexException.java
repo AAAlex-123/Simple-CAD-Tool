7245d86bc596df751217133bd1f39e156ca32531
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-31 16:49:50 +0300

    moved to correct package + doc

--- src/exceptions/InvalidIndexException.java
+++ src/exceptions/InvalidIndexException.java
@@ -1,18 +1,22 @@
-package exceptions;
+package component.exceptions;
 
-import components.Component;
+import component.components.Component;
 
-/** Thrown when an invalid index is used to access parts of a Component. */
+/**
+ * Thrown when an invalid index is used to access a pin of a {@code Component}.
+ *
+ * @author Alex Mandelias
+ */
 public final class InvalidIndexException extends RuntimeException {
 
 	/**
-	 * Constructs the exception using information about the {@code Component} and
-	 * the invalid {@code index}.
-	 * 
-	 * @param c     the Component that is accessed
-	 * @param index the invalid index
+	 * Constructs the Exception with a {@code Component} and the invalid
+	 * {@code index}.
+	 *
+	 * @param component the Component that is accessed
+	 * @param index     the invalid index
 	 */
-	public InvalidIndexException(Component c, int index) {
-		super(String.format("Invalid index %d for component of type %s.", index, c)); //$NON-NLS-1$
+	public InvalidIndexException(Component component, int index) {
+		super(String.format("Invalid index %d for Component of type %s", index, component)); //$NON-NLS-1$
 	}
 }
