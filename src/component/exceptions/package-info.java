ef96fe5dd605b32aac6b81236011fde621747d90
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-08-31 17:06:37 +0300

    moved to correct package + doc

---
+++ src/component/exceptions/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Defines a set of exceptions regarding the {@link component.components} and
+ * the {@link component.graphics} packages.
+ *
+ * @author Alex Mandelias
+ */
+package component.exceptions;
