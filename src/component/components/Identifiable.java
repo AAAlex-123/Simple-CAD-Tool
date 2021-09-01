0c923fde4f2d73af4e00d9f46108c8f53c28802a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-01 18:18:00 +0300

    doc + equals method

--- src/component/components/Identifiable.java
+++ src/component/components/Identifiable.java
@@ -1,11 +1,13 @@
 package component.components;
 
+import java.util.Objects;
+
 /**
- * An Interface for objects that can be identified.
+ * An Interface for objects that can be identified by an ID.
  *
  * @param <T> the type of the ID
  *
- * @author alexm
+ * @author Alex Mandelias
  */
 public interface Identifiable<T> {
 
@@ -22,4 +24,24 @@ public interface Identifiable<T> {
 	 * @param id the new ID
 	 */
 	void setID(T id);
+
+	/**
+	 * Returns whether or not two Identifiable objects have the same ID.
+	 *
+	 * @param <T> the type of the objects' ID
+	 * @param i1  the first Identifiable object
+	 * @param i2  the second Identifiable object
+	 *
+	 * @return {@code true} if the ID of the first object is equal to the ID of the
+	 *         second object, {@code false} otherwise
+	 *
+	 * @throws NullPointerException if any argument is {@code null}
+	 *
+	 * @see Objects#equals
+	 */
+	static <T> boolean equals(Identifiable<T> i1, Identifiable<T> i2) {
+		Objects.requireNonNull(i1, "i1 is null"); //$NON-NLS-1$
+		Objects.requireNonNull(i2, "i2 is null"); //$NON-NLS-1$
+		return Objects.equals(i1.getID(), i2.getID());
+	}
 }
