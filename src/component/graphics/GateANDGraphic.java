81d5299b2d58330ec482c87388fa4c522b7e3a7d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-03 10:28:53 +0300

    doc + works with new ComponentGraphic

--- src/component/graphics/GateANDGraphic.java
+++ src/component/graphics/GateANDGraphic.java
@@ -1,53 +1,39 @@
 package component.graphics;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import application.StringConstants;
-import exceptions.MissingSpriteException;
+import component.components.Component;
 
 /**
- * Handles the Graphics of an {@link GateAND}.
+ * Graphics for a {@link component.ComponentType#GATEAND GATEAND}.
  *
- * @author alexm
+ * @author Alex Mandelias
  */
 final class GateANDGraphic extends GateGraphic {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
 	private static final String sprite = StringConstants.COMPONENT_ICON_PATH
-			+ "gate_and.png"; //$NON-NLS-1$
+	        + "gate_and.png"; //$NON-NLS-1$
 
 	private static final BufferedImage image;
 
 	static {
-		BufferedImage temp = null;
-		File          file = null;
-
-		try {
-			file = new File(sprite);
-			temp = ImageIO.read(file);
-		} catch (IOException e) {
-			throw new MissingSpriteException(file);
-		}
-
-		image = temp;
+		image = ComponentGraphic.loadImage(GateANDGraphic.sprite);
 	}
 
 	/**
-	 * Constructs the graphics object
+	 * Constructs the Graphics object.
 	 *
-	 * @param c the related Component
+	 * @param component the related {@code Component}
 	 */
-	public GateANDGraphic(Component c) {
-		super(c);
+	public GateANDGraphic(Component component) {
+		super(component);
 	}
 
 	@Override
 	protected BufferedImage getImage() {
-		return image;
+		return GateANDGraphic.image;
 	}
 }
