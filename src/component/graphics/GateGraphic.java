d01fd6df0c21f69cc32339f5b1be08579e26c356
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-03 10:28:46 +0300

    doc + works with new ComponentGraphic

--- src/component/graphics/GateGraphic.java
+++ src/component/graphics/GateGraphic.java
@@ -1,100 +1,51 @@
 package component.graphics;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import application.StringConstants;
-import exceptions.MissingSpriteException;
+import component.components.Component;
 
 /**
- * Handles the Graphics of a {@link Gate}.
+ * Graphics for a {@link component.ComponentType#GATE GATE}.
  *
- * @author alexm
+ * @author Alex Mandelias
  */
 class GateGraphic extends ComponentGraphic {
 
 	private static final long serialVersionUID = 1L;
 
 	private static final String sprite = StringConstants.COMPONENT_ICON_PATH
-			+ "gate.png"; //$NON-NLS-1$
+	        + "gate.png"; //$NON-NLS-1$
 
 	private static final BufferedImage image;
 
 	static {
-		BufferedImage temp = null;
-		File          file = null;
-
-		try {
-			file = new File(sprite);
-			temp = ImageIO.read(file);
-		} catch (IOException e) {
-			throw new MissingSpriteException(file);
-		}
-
-		image = temp;
-	}
-
-	// only for custom gates
-	private final String description;
-
-	/**
-	 * Constructs the graphics object
-	 *
-	 * @param c the related Component
-	 */
-	public GateGraphic(Component c) {
-		this(c, ""); //$NON-NLS-1$
+		image = ComponentGraphic.loadImage(GateGraphic.sprite);
 	}
 
 	/**
-	 * Constructs the graphics object
+	 * Constructs the Graphics object.
 	 *
-	 * @param c    the related Component
-	 * @param desc the Component's description
+	 * @param component the related {@code Component}
 	 */
-	public GateGraphic(Component c, String desc) {
-		super(c);
-		description = desc;
+	public GateGraphic(Component component) {
+		super(component);
 	}
 
 	@Override
 	protected final void draw(Graphics g) {
-		drawSprite(g);
-
-		g.setColor(Color.BLACK);
-		g.drawString(description, 7, (getHeight() / 2) + 5);
+		g.drawImage(getImage(), 0, 0, null);
 	}
 
-	/**
-	 * Draws the sprite of the Gate.
-	 *
-	 * @param g the Graphics object with which to draw
-	 */
-	protected final void drawSprite(Graphics g) {
-		BufferedImage bImage = getImage();
-		if (bImage != null)
-			g.drawImage(bImage, 0, 0, null);
-		else
-			g.drawImage(image, 0, 0, null);
-	}
-
-	/**
-	 * Returns the Image used to draw the Primitive Gates. Defaults to plain grey
-	 * rectangle used for user-made Gates.
-	 *
-	 * @return the Image
-	 */
+	/** @implNote the default implementation returns a grey rectangle */
+	@Override
 	protected BufferedImage getImage() {
-		return image;
+		return GateGraphic.image;
 	}
 
 	@Override
 	protected void attachListeners() {
-		attachListeners_(DRAG_KB_FOCUS);
+		attachListenersByFlags(ComponentGraphic.DRAG_KB_FOCUS);
 	}
 }
