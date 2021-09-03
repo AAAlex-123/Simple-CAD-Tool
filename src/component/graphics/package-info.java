e102a5c36f4edc8a8fe1ea07aa9399d1fdc50d7f
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-03 10:35:40 +0300

    Create package-info.java

---
+++ src/component/graphics/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Defines a hierarchy of {@link component.graphics.ComponentGraphic Graphic}
+ * objects which provide users with a way to interact and visualise
+ * {@link component.components.Component Components} via a GUI.
+ * <p>
+ * {@code Graphic} objects can only be obtained using the static method
+ * {@code forComponent(Component)} defined in the {@code Graphic} class.
+ *
+ * @author Alex Mandelias
+ *
+ * @see component.graphics.ComponentGraphic#forComponent(component.components.Component)
+ *      ComponentGraphic#forComponent(Component)
+ */
+package component.graphics;
