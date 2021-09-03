b66fd72f127f0bf1c1a28c110d30c412959b0bf3
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-03 10:37:23 +0300

    Graphics (#73)
     - Moved everything to correct package
     - Updated ComponentGraphic.java and added default implementation notes
       - doc
       - better drawing:
         - pushed a lot of stuff to ComponentGraphic
         - extending ComponentGraphic is now simpler
       - used GraphicHook
     - Updated doc of everything, made subclasses work with new ComponentGraphic
     - Created PrimitiveGateGraphic.java
     - Created package-info.java

---
+++ src/component/graphics/BranchGraphic.java
@@ -0,0 +1,80 @@
+package component.graphics;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import component.components.Component;
+import component.components.GraphicHook;
+
+/**
+ * Graphics for a {@link component.ComponentType#BRANCH BRANCH}.
+ *
+ * @author Alex Mandelias
+ */
+final class BranchGraphic extends ComponentGraphic {
+
+	private static final long serialVersionUID = 1L;
+
+	// graphics, 1 or -1, slope of the line
+	//  1 = draw top-left to bottom-right
+	// -1 = draw bottom-left to top-right
+	private int direction;
+
+	/**
+	 * Constructs the Graphics object.
+	 *
+	 * @param component the related {@code Component}
+	 */
+	public BranchGraphic(Component component) {
+		super(component);
+	}
+
+	@Override
+	protected void attachListeners() {
+		attachListenersByFlags((byte) 0);
+	}
+
+	@Override
+	protected void draw(Graphics g) {
+		g.setColor(GraphicHook.getActive(component, 0) ? Color.green : Color.red);
+
+		// draw with correct direction (as specified in its declaration)
+		if (direction == 1)
+			g.drawLine(5, 5, getWidth() - 5, getHeight() - 6);
+		else if (direction == -1)
+			g.drawLine(5, getHeight() - 6, getWidth() - 5, 5);
+		else
+			throw new RuntimeException("Invalid Branch direction"); //$NON-NLS-1$
+	}
+
+	@Override
+	protected void drawPins(Graphics g) {}
+
+	@Override
+	protected void drawID(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.drawString(component.getID(), (getWidth() / 2) - 4, (getHeight() / 2) + 5);
+	}
+
+	@Override
+	protected BufferedImage getImage() {
+		return null;
+	}
+
+	@Override
+	protected void updateOnMovement() {
+		// from the new coordinates calculate the Branch's start point, width and height
+		// and also calculate its direction (as specified in its declaration).
+		final Component in  = GraphicHook.getInputs(component).get(0);
+		final Component out = GraphicHook.getOutputs(component).get(0).get(0);
+		final Point     p1  = in.getGraphics().getBranchInputCoords(component);
+		final Point     p2  = out.getGraphics().getBranchOutputCoords(component);
+		direction = ((p2.x - p1.x) * (p2.y - p1.y)) > 0 ? 1 : -1;
+		// components with a dimension = 0 aren't drawn and text can't be drawn on a
+		// small space, so extra width/height is added here and removed it when drawing
+		setBounds(Math.min(p1.x, p2.x) - 5, Math.min(p1.y, p2.y) - 5, Math.abs(p2.x - p1.x) + 11,
+		        Math.abs(p2.y - p1.y) + 11);
+	}
+}
---
+++ src/component/graphics/GateGraphic.java
@@ -0,0 +1,45 @@
+package component.graphics;
+
+import java.awt.image.BufferedImage;
+
+import application.StringConstants;
+import component.components.Component;
+
+/**
+ * Graphics for a {@link component.ComponentType#GATE GATE}.
+ *
+ * @author Alex Mandelias
+ */
+class GateGraphic extends ComponentGraphic {
+
+	private static final long serialVersionUID = 2L;
+
+	private static final String sprite = StringConstants.COMPONENT_ICON_PATH
+	        + "gate.png"; //$NON-NLS-1$
+
+	private static final BufferedImage image;
+
+	static {
+		image = ComponentGraphic.loadImage(GateGraphic.sprite);
+	}
+
+	/**
+	 * Constructs the Graphics object.
+	 *
+	 * @param component the related {@code Component}
+	 */
+	public GateGraphic(Component component) {
+		super(component);
+	}
+
+	/** @implNote the default implementation returns a grey rectangle */
+	@Override
+	protected BufferedImage getImage() {
+		return GateGraphic.image;
+	}
+
+	@Override
+	protected void attachListeners() {
+		attachListenersByFlags(ComponentGraphic.DRAG_KB_FOCUS);
+	}
+}
