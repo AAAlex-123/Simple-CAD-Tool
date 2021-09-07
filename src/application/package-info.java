388e848ae88723b2a1c28717837046b76fa1856e
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-07 17:52:10 +0300

    added author

--- src/application/package-info.java
+++ src/application/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Defines a set of classes that together create an interactive Application with
  * a UI. The user may run and use it to see their dream circuit come to life.
+ *
+ * @author Alex Mandelias
  */
 package application;
