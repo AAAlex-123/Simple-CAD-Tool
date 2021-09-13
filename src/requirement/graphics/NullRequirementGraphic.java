01aed5e4961a16cf58aa2fdf685719d3dce5d762
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-09-13 10:13:00 +0300

    added icon to indicate info/error

--- src/requirement/graphics/NullRequirementGraphic.java
+++ src/requirement/graphics/NullRequirementGraphic.java
@@ -1,9 +1,12 @@
 package requirement.graphics;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 import localisation.Languages;
 import requirement.requirements.AbstractRequirement;
@@ -17,9 +20,11 @@ import requirement.requirements.AbstractRequirement;
  * @author alexm
  */
 public class NullRequirementGraphic
-        extends AbstractRequirementGraphic<AbstractRequirement> {
+extends AbstractRequirementGraphic<AbstractRequirement> {
 
-	private final JLabel key, explanation;
+	private final JPanel mainPanel;
+	private final JLabel icon, key, explanation;
+
 
 	/**
 	 * Constructs the NullGraphic.
@@ -27,18 +32,23 @@ public class NullRequirementGraphic
 	 * @param requirement the Requirement that doesn't have a Graphic
 	 * @param reason      the reason that a Graphic isn't supported
 	 */
-	public NullRequirementGraphic(AbstractRequirement requirement, String reason) {
+	public NullRequirementGraphic(AbstractRequirement requirement, String reason, boolean error) {
 		super(requirement);
-		setLayout(new GridLayout(2, 1, 10, 0));
+		setLayout(new BorderLayout());
 
+		icon = new JLabel(UIManager.getIcon("OptionPane." + (error ? "error" : "information") + "Icon"));
 		key = new JLabel(String.format(Languages.getString("NullRequirementGraphic.0"), requirement.key())); //$NON-NLS-1$
 		explanation = new JLabel(String.format(Languages.getString("NullRequirementGraphic.1"), reason)); //$NON-NLS-1$
 
 		key.setHorizontalAlignment(SwingConstants.CENTER);
 		explanation.setHorizontalAlignment(SwingConstants.CENTER);
 
-		add(key);
-		add(explanation);
+		mainPanel = new JPanel(new GridLayout(2, 1, 10, 0));
+		mainPanel.add(key);
+		mainPanel.add(explanation);
+
+		add(icon, BorderLayout.WEST);
+		add(mainPanel, BorderLayout.CENTER);
 	}
 
 	@Override
